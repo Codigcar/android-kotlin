759508452cc72f5c19d68698d9fca369068b7897
Carlos Castilla <carlos.castilla009@gmail.com>
2023-09-15 03:18:50 -0500

    feat(compose-celcius): add material theme

--- compose/orreily_project1_celcius/app/src/main/java/com/example/myapplication/myapplication/converterapp/MainActivity.kt
+++ compose/orreily_project1_celcius/app/src/main/java/com/example/myapplication/myapplication/converterapp/MainActivity.kt
@@ -5,16 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -22,7 +20,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PreviewMainActivity()
+            MaterialTheme {
+                Surface {
+                    PreviewMainActivity()
+                }
+            }
         }
     }
 }
@@ -107,7 +109,7 @@ fun InputTemperate(temperature: String, changed: (String) -> Unit) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "main", device = Devices.PIXEL_2)
 @Composable
 fun PreviewMainActivity() {
     val celsius = remember { mutableStateOf(0) }
